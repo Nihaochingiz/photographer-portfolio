2850192302658cef27199ad2a0d44eed3aef5d6c
Ilya Fofanov <66513936+Nihaochingiz@users.noreply.github.com>
2022-02-13 20:10:59 +0300

    add slider
    not optimal way, I used id, not data-types

--- script.js
+++ script.js
@@ -11,5 +11,58 @@ window.onload = function() {
 
 
     });
-   
+
+
 }
+
+
+
+const btnWinter = document.getElementById('winter');
+const btnSpring= document.getElementById('spring');
+const btnSummer = document.getElementById('summer');
+const btnAutumn = document.getElementById('autumn');
+const portfolioImages = document.querySelectorAll('.item-portfolio-container');
+
+
+
+
+const allBtns = [btnWinter,btnAutumn,btnSpring,btnSummer];
+
+
+
+
+
+function changeImage(event) {
+
+
+ 
+           if (event.target.id === 'winter') {
+        portfolioImages.forEach((img, index) => img.src = `./assets/img/${event.target.id}/${index + 1}.jpg`);
+                }
+          else if((event.target.id === 'summer')){
+        portfolioImages.forEach((img, index) => img.src = `./assets/img/${event.target.id}/${index + 1}.jpg`);
+        }
+        else if((event.target.id === 'spring')){
+            portfolioImages.forEach((img, index) => img.src = `./assets/img/${event.target.id}/${index + 1}.jpg`);
+            }
+        else if((event.target.id === 'autumn')){
+                portfolioImages.forEach((img, index) => img.src = `./assets/img/${event.target.id}/${index + 1}.jpg`);
+                }
+    
+}
+
+
+
+
+
+allBtns.forEach(item => {
+    
+    item.addEventListener('click', changeImage);
+}
+
+)
+
+
+
+
+  
